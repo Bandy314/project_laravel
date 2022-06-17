69c6f652b9cc86f41a7b9c06b003d3b4fb6d2c7d
Bandy314 <sanya.borodin2002@bk.ru>
2022-06-17 14:12:00 +0500

    new_laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='public/css/boostrap.css'>
+    <title>Проект</title>
+</head>
+<body>
+    
+</body>
+</html>
